aa137541237ae41dfc5cd0c08191220da1e65f89
H-chai <143179912+H-chai@users.noreply.github.com>
2025-03-07 11:53:32 +0100

    Set up Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
 
 function App() {
-  return <div>This is my portfolio</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

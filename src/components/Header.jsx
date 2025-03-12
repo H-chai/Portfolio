84d91da607b028c25248a7df51517a051793ec35
H-chai <143179912+H-chai@users.noreply.github.com>
2025-03-12 12:03:50 +0100

    Header css with responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import styles from '../styles/Header.module.css';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+
 export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenuButton() {
+    setIsMenuOpen((prev) => !prev);
+  }
+
   return (
     <header>
-      <div>PORTFOLIO</div>
-      <nav>
+      <div className={styles.pageTitle}>HØ</div>
+      <nav className={styles.navMenu}>
+        <ul className={styles.navMenuList}>
+          <li>PROJECTS</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+      {isMenuOpen ? (
+        <div>
+          <button onClick={toggleMenuButton} className={styles.toggleIcon}>
+            <CloseIcon></CloseIcon>
+          </button>
+        </div>
+      ) : (
+        <button onClick={toggleMenuButton} className={styles.toggleIcon}>
+          <MenuOutlinedIcon></MenuOutlinedIcon>
+        </button>
+      )}
+      <nav
+        className={`${styles.navResponsive} ${isMenuOpen ? styles.open : ''}`}
+      >
         <ul>
           <li>PROJECTS</li>
           <li>ABOUT</li>

0dc0fed7523a7c027b3fe16db19d81d5ec1e3bb9
H-chai <143179912+H-chai@users.noreply.github.com>
2025-03-17 11:10:02 +0100

    Add back to home

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useStore } from '../store/projectStore';
 import styles from '../styles/Project.module.css';
+import { Link } from 'react-router-dom';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 
 export function Project() {
   const { id } = useParams();
@@ -13,45 +15,51 @@ export function Project() {
 
   return (
     <div className={styles.projectPage}>
-      <div className={styles.projectInfo}>
-        <h1 className={styles.projectTitle}>{project.name}</h1>
-        <div className={styles.tagContainer}>
-          {project.tags.map((tag) => (
-            <p key={tag} className={styles.tag}>
-              {tag}
-            </p>
-          ))}
-        </div>
-
-        <ul className={styles.list}>
-          <li className={styles.listItem}>
-            <p className={styles.listTitle}>Goal</p>
-            <p className={styles.listContent}>{project.goal}</p>
-          </li>
-          <li className={styles.listItem}>
-            <p className={styles.listTitle}>Technologies</p>
-            {project.technologies.map((tech) => (
-              <p key={tech} className={styles.listContent}>
-                {tech}
+      <div className={styles.projectContainer}>
+        <div className={styles.projectInfo}>
+          <h1 className={styles.projectTitle}>{project.name}</h1>
+          <div className={styles.tagContainer}>
+            {project.tags.map((tag) => (
+              <p key={tag} className={styles.tag}>
+                {tag}
               </p>
             ))}
-          </li>
-          <li className={styles.listItem}>
-            <p className={styles.listTitle}>Year</p>
-            <p className={styles.listContent}>{project.year}</p>
-          </li>
-        </ul>
+          </div>
+
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <p className={styles.listTitle}>Goal</p>
+              <p className={styles.listContent}>{project.goal}</p>
+            </li>
+            <li className={styles.listItem}>
+              <p className={styles.listTitle}>Technologies</p>
+              {project.technologies.map((tech) => (
+                <p key={tech} className={styles.listContent}>
+                  {tech}
+                </p>
+              ))}
+            </li>
+            <li className={styles.listItem}>
+              <p className={styles.listTitle}>Year</p>
+              <p className={styles.listContent}>{project.year}</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.imageContainer}>
+          {project.medias.map((media) => (
+            <img
+              src={media.url}
+              alt={media.alt}
+              key={media.alt}
+              className={`${styles.image}`}
+            />
+          ))}
+        </div>
       </div>
-      <div className={styles.imageContainer}>
-        {project.medias.map((media) => (
-          <img
-            src={media.url}
-            alt={media.alt}
-            key={media.alt}
-            className={`${styles.image}`}
-          />
-        ))}
-      </div>
+      <Link to="/" className={styles.backHome}>
+        <KeyboardBackspaceIcon></KeyboardBackspaceIcon>
+        <p>Go back to home</p>
+      </Link>
     </div>
   );
 }
